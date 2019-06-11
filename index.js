6460ca22c07acba3064dd2ad48ba9a18d4180369
valeria campilongo <cvaleriac@hotmail.it>
2019-06-11 17:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,29 @@ function produceDrivingRange(blockRange) {
   return function(startBlock, endblock) {
   let start = parsInt(startBlock);
   let end = parsInt(endblock)
-  };
+  let result = 0;
+      start>=end ? result = start - end : result = start - end
+      if (result <= blockRange) {
+          return `within range by ${blockRange - result}`;
+      } else {
+          return `${result - blockRange} blocks out of range`;
+      }  
+  }
 }
+
+function produceTipCalculator(tip) {
+  return function(percentage) {
+      return percentage * tip;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class Driver {
+      constructor(name) {
+          this.name = name;
+          this.id = ++driverId;
+      }
+  }
+}
+  
